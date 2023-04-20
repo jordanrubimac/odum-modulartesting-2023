2f21cab6c6e8a935ce7f2deeccdf622884c1f21d
Jordan Rubin-McGregor <rubinmjl@miamioh.edu>
2023-04-20 09:28:14 -0500

    initial commit

---
+++ R/analyzedata.R
@@ -0,0 +1,33 @@
+MILES_TO_KM = 1.609
+
+#' @export
+busiest_routes = function (dataframe, startcol, endcol) {
+  stopifnot(all({{dataframe}}$Passengers >= 1))
+  stopifnot(all(!is.na({{dataframe}}$Passengers)))
+
+  pairs = group_by({{dataframe}}, {{startcol}}, {{endcol}}) %>%
+    summarize(Passengers=sum(Passengers), distance_km=first(Distance) * MILES_TO_KM)
+  arrange(pairs, -Passengers)
+
+  pairs = mutate(pairs,
+                 airport1 = if_else({{startcol}} < {{endcol}}, {{startcol}}, {{endcol}}),
+                 airport2 = if_else({{startcol}} < {{endcol}}, {{endcol}}, {{startcol}}))
+
+  pairs = group_by(pairs, airport1, airport2) %>% summarize(
+    Passengers=sum(Passengers),
+    distance_km=first(distance_km))
+
+  return(arrange(pairs, -Passengers))
+}
+
+#' @export
+market_shares = function(dataframe, carrier, origin) {
+  mkt_shares = group_by({{dataframe}}, {{carrier}}, {{origin}}) %>%
+    summarize(Passengers=sum(Passengers)) %>%
+    group_by({{origin}}) %>%
+    mutate(market_share=Passengers/sum(Passengers), total_passengers=sum(Passengers)) %>%
+    ungroup()
+
+  result = arrange(mkt_shares, -market_share)
+  return(result)
+}
---
+++ R/cleandata.R
@@ -0,0 +1,11 @@
+#' @export
+data_clean = function(datafile, cityfile, carrierfile) {
+  data = readr::read_csv({{datafile}})
+  market_ids = readr::read_csv({{cityfile}})
+  data = dplyr::left_join(data, dplyr::rename(market_ids, OriginCity="Description"), by=c(OriginCityMarketID="Code"))
+  data = dplyr::left_join(data, dplyr::rename(market_ids, DestCity="Description"), by=c(DestCityMarketID="Code"))
+
+  carriers = readr::read_csv({{carrierfile}})
+  data = dplyr::left_join(data, dplyr::rename(carriers, OperatingCarrierName="Description"), by=c(OpCarrier="Code"))
+  data = dplyr::left_join(data, dplyr::rename(carriers, TicketingCarrierName="Description"), by=c(TkCarrier="Code"))
+}
